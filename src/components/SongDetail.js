9c6c3ef4eb7f54b5b36d46759bf57c530973a6b8
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-31 00:27:08 -0700

    react

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const SongDetail = () => {
+const SongDetail = (props) => {
+  console.log(props);
   return <div>Song Detail</div>
 };
 
