1f01b357f32cc7711cad6ad7711b316dca895ede
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-31 00:28:02 -0700

    react

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -3,11 +3,11 @@ import { connect } from "react-redux";
 
 const SongDetail = (props) => {
   console.log(props);
-  return <div>Song Detail</div>
+  return <div>Song Detail</div>;
 };
 
 const mapStateToProps = (state) => {
-  return { song: state.selectedSong }
+  return { song: state.selectedSong };
 };
 
 export default connect(mapStateToProps)(SongDetail);
